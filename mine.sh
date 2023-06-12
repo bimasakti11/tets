d829632aafe66b932fd185baa3f03ee9db2e585b
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-12 21:12:22 +0900

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r.azure -p x --cpu "$(nproc)"
