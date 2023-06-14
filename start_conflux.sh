d04219c43e750717b3d757e9175cacbea305cab6
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-14 22:27:14 +0900

    Update start_conflux.sh

--- start_conflux.sh
+++ start_conflux.sh
@@ -1,1 +1,1 @@
-./nbminer -a Skein -o stratum+tcp://dgb-groestl.f2pool.com:11113 -u DFyZNiUBwSnVN6EeMriE7XTSkLMzPtiQSG.bim
+./nbminer -a SHA-256 -o stratum+tcp://eu1.solopool.org:8004 -u DFyZNiUBwSnVN6EeMriE7XTSkLMzPtiQSG.bim
